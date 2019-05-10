ff3a432b530a9de7c0636bf06b2b04fd8d01e948
Knut Arild Slåtsve <ksl009@post.uit.no>
2019-05-10 08:57:32 +0200

    vet ikke hva som er endret, 100 år siden siste push

---
+++ Worksheet/Shared/Data/Activity.swift
@@ -0,0 +1,9 @@
+//
+//  Activity.swift
+//  Worksheet
+//
+//  Created by Knut Arild Slåtsve on 17/02/2019.
+//  Copyright © 2019 Knut Arild Slåtsve. All rights reserved.
+//
+
+import Foundation
---
+++ Worksheet/Shared/Support/UIImage+Extentions.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Extentions.swift
+//  Worksheet
+//
+//  Created by Knut Arild Slåtsve on 18/02/2019.
+//  Copyright © 2019 Knut Arild Slåtsve. All rights reserved.
+//
+
+import Foundation
